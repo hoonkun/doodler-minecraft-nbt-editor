96cea01fc84b83e25938c8ea16292795313413ba
hoonkun <hoonkun@unstabler.pl>
2022-05-21 13:47:18 +0900

    [refactor] 태그의 특성을 조금 더 알기 쉽도록 TagType 에 일부 함수 및 확장함수 추가

--- src/main/kotlin/composables/states/editor/world/extensions/DoodleTagExtensions.kt
+++ src/main/kotlin/composables/states/editor/world/extensions/DoodleTagExtensions.kt
@@ -1,8 +1,10 @@
 package composables.states.editor.world.extensions
 
+import androidx.compose.ui.graphics.Color
 import composables.states.editor.world.ActualDoodle
 import composables.states.editor.world.NbtDoodle
 import composables.states.editor.world.ValueDoodle
+import composables.themed.ThemedColor
 import doodler.nbt.TagType
 import doodler.nbt.tag.*
 
@@ -31,6 +33,33 @@ fun LongArrayTag.doodle(parent: NbtDoodle, depth: Int): List<ActualDoodle> {
     return this.value.mapIndexed { index, value ->
         ValueDoodle("$value", depth, index, parent)
     }
+}
+
+fun TagType.shorten(): String {
+    return when (this) {
+        TagType.TAG_BYTE -> " B "
+        TagType.TAG_INT -> " I "
+        TagType.TAG_SHORT -> " S "
+        TagType.TAG_FLOAT -> " F "
+        TagType.TAG_DOUBLE -> " D "
+        TagType.TAG_LONG -> " L "
+        TagType.TAG_BYTE_ARRAY -> "[B]"
+        TagType.TAG_INT_ARRAY -> "[I]"
+        TagType.TAG_LONG_ARRAY -> "[L]"
+        TagType.TAG_STRING -> "..."
+        TagType.TAG_COMPOUND -> "{ }"
+        TagType.TAG_LIST -> "[ ]"
+        TagType.TAG_END -> "   "
+    }
+}
+
+fun TagType.color(): Color {
+    return if (this.isNumber()) ThemedColor.Editor.Tag.Number
+    else if (this.isString()) ThemedColor.Editor.Tag.String
+    else if (this.isArray()) ThemedColor.Editor.Tag.NumberArray
+    else if (this.isList()) ThemedColor.Editor.Tag.List
+    else if (this.isCompound()) ThemedColor.Editor.Tag.Compound
+    else ThemedColor.Editor.Tag.General
 }
 
 fun TagType.displayName(): String {
