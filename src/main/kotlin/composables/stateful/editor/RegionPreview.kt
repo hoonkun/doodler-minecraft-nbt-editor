73f7fd6a3650c6988b3fffc11a53543316f60748
hoonkun <hoonkun@unstabler.pl>
2022-05-23 07:55:58 +0900

    [refactor] Surface 를 만드는 과정이 UI를 블락하지 않도록 함

--- src/main/kotlin/composables/stateful/editor/RegionPreview.kt
+++ src/main/kotlin/composables/stateful/editor/RegionPreview.kt
@@ -5,27 +5,29 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.zIndex
 import doodler.anvil.*
 import doodler.file.WorldTree
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jetbrains.skia.*
 import org.jetbrains.skiko.toBufferedImage
 
 @Composable
 fun BoxScope.RegionPreview(tree: WorldTree, location: AnvilLocation = AnvilLocation(0, -1)) {
-    var byteList by remember { mutableStateOf(ByteArray(0)) }
+    var map by remember { mutableStateOf<ImageBitmap?>(null) }
 
     LaunchedEffect(location) {
-        launch {
+        withContext(Dispatchers.IO) {
             val bytes = tree.overworld.region.find { it.name == "r.${location.x}.${location.z}.mca" }?.readBytes()
-                ?: return@launch
+                ?: return@withContext
             val subChunks = AnvilWorker.loadChunksWith(bytes) { chunkLoc, tag ->
                 Pair(chunkLoc, SurfaceWorker.createSubChunk(tag))
             }
-            val temp = ByteArray(512 * 512 * 4) { index -> if (index % 4 == 2 || index % 4 == 3) -1 else 0 }
+            val pixels = ByteArray(512 * 512 * 4) { index -> if (index % 4 == 2 || index % 4 == 3) -1 else 0 }
             subChunks.forEach { (loc, chunks) ->
                 val baseX = loc.x * 16
                 val baseZ = loc.z * 16
@@ -34,16 +36,24 @@ fun BoxScope.RegionPreview(tree: WorldTree, location: AnvilLocation = AnvilLocat
                     val x = baseX + (index / 16)
                     val z = baseZ + (index % 16)
 
-                    temp[(x * 512 + z) * 4 + 0] = block.color[2]
-                    temp[(x * 512 + z) * 4 + 1] = block.color[1]
-                    temp[(x * 512 + z) * 4 + 2] = block.color[0]
-                    temp[(x * 512 + z) * 4 + 3] = block.color[3]
+                    pixels[(x * 512 + z) * 4 + 0] = block.color[2]
+                    pixels[(x * 512 + z) * 4 + 1] = block.color[1]
+                    pixels[(x * 512 + z) * 4 + 2] = block.color[0]
+                    pixels[(x * 512 + z) * 4 + 3] = block.color[3]
                 }
             }
-            byteList = temp
+            map = Bitmap()
+                .apply {
+                    allocPixels(ImageInfo(512, 512, ColorType.N32, ColorAlphaType.OPAQUE))
+                    installPixels(pixels)
+                }
+                .toBufferedImage()
+                .toComposeImageBitmap()
             delay(3000)
         }
     }
+
+    if (map == null) return
 
     Box(
         modifier = Modifier
@@ -53,10 +63,7 @@ fun BoxScope.RegionPreview(tree: WorldTree, location: AnvilLocation = AnvilLocat
             .zIndex(0f)
     ) {
         Image(
-            Bitmap().apply {
-                allocPixels(ImageInfo(512, 512, ColorType.N32, ColorAlphaType.OPAQUE))
-                installPixels(byteList)
-            }.toBufferedImage().toComposeImageBitmap(),
+            map!!,
             null,
             filterQuality = androidx.compose.ui.graphics.FilterQuality.None,
             modifier = Modifier.fillMaxSize()
