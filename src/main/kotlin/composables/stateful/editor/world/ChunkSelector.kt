852c912e0714147c9a332329d435a86940b6e074
hoonkun <hoonkun@unstabler.pl>
2022-05-27 13:26:04 +0900

    [refactor] ChunkSelector 와 관련된 stateless 컴포저블들을 ChunkSelector.kt 로 이동

--- src/main/kotlin/composables/stateful/editor/world/ChunkSelector.kt
+++ src/main/kotlin/composables/stateful/editor/world/ChunkSelector.kt
@@ -9,6 +9,8 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -17,16 +19,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.TransformedText
@@ -34,9 +40,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import composables.stateful.editor.*
-import composables.stateless.editor.ChunkSelectorDropdown
-import composables.stateless.editor.CoordinateInput
-import composables.stateless.editor.CoordinateText
 import composables.themed.JetBrainsMono
 import composables.themed.ThemedColor
 import doodler.editor.McaEditor
@@ -618,5 +621,92 @@ fun <T>SelectorDropdown(
                 Spacer(modifier = Modifier.height(10.dp))
             }
         }
+    }
+}
+
+@Composable
+fun RowScope.CoordinateText(text: String, invalid: Boolean = false) {
+    Text(
+        text,
+        fontSize = 16.sp,
+        color = if (invalid) ThemedColor.Editor.Selector.Invalid else ThemedColor.Editor.Tag.General,
+        fontFamily = JetBrainsMono,
+        modifier = Modifier.focusable(false)
+    )
+}
+
+@Composable
+fun RowScope.CoordinateInput(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    transformer: (AnnotatedString) -> TransformedText = { TransformedText(it, OffsetMapping.Identity) },
+    disabled: Boolean = false
+) {
+    BasicTextField(
+        value,
+        onValueChange,
+        textStyle = TextStyle(
+            fontSize = 16.sp,
+            color = ThemedColor.Editor.Tag.General,
+            fontFamily = JetBrainsMono
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true,
+        cursorBrush = SolidColor(ThemedColor.Editor.Tag.General),
+        visualTransformation = transformer,
+        enabled = !disabled,
+        modifier = Modifier
+            .width((value.text.length.coerceAtLeast(1) * 9.75).dp)
+            .focusable(false)
+            .onFocusChanged {
+                if (!it.isFocused && value.text.isEmpty()) onValueChange(TextFieldValue("-"))
+            }
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@Composable
+fun ChunkSelectorDropdown(
+    prefix: String,
+    accent: Boolean = false,
+    valid: Boolean = true,
+    modifier: Modifier = Modifier,
+    disabled: Boolean = false,
+    onClick: (MouseClickScope.() -> Unit)? = null,
+    content: @Composable RowScope.(Boolean) -> Unit
+) {
+    var hover by remember { mutableStateOf(false) }
+
+    Row (
+        modifier = Modifier.then(modifier)
+            .background(
+                ThemedColor.from(
+                    ThemedColor.Editor.Selector.background(accent, valid, onClick != null && hover),
+                    alpha = if (disabled) (255 * 0.6f).toInt() else 255
+                ),
+                RoundedCornerShape(4.dp)
+            )
+            .height(40.dp)
+            .alpha(if (disabled) 0.6f else 1f)
+            .let {
+                if (onClick != null)
+                    it.mouseClickable(onClick = onClick)
+                        .onPointerEvent(PointerEventType.Enter) { hover = true }
+                        .onPointerEvent(PointerEventType.Exit) { hover = false }
+                else it
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            prefix,
+            fontSize = 14.sp,
+            color = ThemedColor.Editor.Selector.Normal,
+            fontFamily = JetBrainsMono
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        content(disabled)
+        Spacer(modifier = Modifier.width(12.dp))
     }
 }
--- src/main/kotlin/composables/stateless/editor/WorldEditorStatelessComposables.kt
+++ src/main/kotlin/composables/stateless/editor/WorldEditorStatelessComposables.kt
@@ -1,31 +1,11 @@
 package composables.stateless.editor
 
-import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.OffsetMapping
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import composables.themed.*
@@ -33,93 +13,6 @@ import java.awt.Desktop
 import java.net.URI
 import java.util.*
 
-
-@Composable
-fun RowScope.CoordinateText(text: String, invalid: Boolean = false) {
-    Text(
-        text,
-        fontSize = 16.sp,
-        color = if (invalid) ThemedColor.Editor.Selector.Invalid else ThemedColor.Editor.Tag.General,
-        fontFamily = JetBrainsMono,
-        modifier = Modifier.focusable(false)
-    )
-}
-
-@Composable
-fun RowScope.CoordinateInput(
-    value: TextFieldValue,
-    onValueChange: (TextFieldValue) -> Unit,
-    transformer: (AnnotatedString) -> TransformedText = { TransformedText(it, OffsetMapping.Identity) },
-    disabled: Boolean = false
-) {
-    BasicTextField(
-        value,
-        onValueChange,
-        textStyle = TextStyle(
-            fontSize = 16.sp,
-            color = ThemedColor.Editor.Tag.General,
-            fontFamily = JetBrainsMono
-        ),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        singleLine = true,
-        cursorBrush = SolidColor(ThemedColor.Editor.Tag.General),
-        visualTransformation = transformer,
-        enabled = !disabled,
-        modifier = Modifier
-            .width((value.text.length.coerceAtLeast(1) * 9.75).dp)
-            .focusable(false)
-            .onFocusChanged {
-                if (!it.isFocused && value.text.isEmpty()) onValueChange(TextFieldValue("-"))
-            }
-    )
-}
-
-@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
-@Composable
-fun ChunkSelectorDropdown(
-    prefix: String,
-    accent: Boolean = false,
-    valid: Boolean = true,
-    modifier: Modifier = Modifier,
-    disabled: Boolean = false,
-    onClick: (MouseClickScope.() -> Unit)? = null,
-    content: @Composable RowScope.(Boolean) -> Unit
-) {
-    var hover by remember { mutableStateOf(false) }
-
-    Row (
-        modifier = Modifier.then(modifier)
-            .background(
-                ThemedColor.from(
-                    ThemedColor.Editor.Selector.background(accent, valid, onClick != null && hover),
-                    alpha = if (disabled) (255 * 0.6f).toInt() else 255
-                ),
-                RoundedCornerShape(4.dp)
-            )
-            .height(40.dp)
-            .alpha(if (disabled) 0.6f else 1f)
-            .let {
-                if (onClick != null)
-                    it.mouseClickable(onClick = onClick)
-                        .onPointerEvent(PointerEventType.Enter) { hover = true }
-                        .onPointerEvent(PointerEventType.Exit) { hover = false }
-                else it
-            },
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
-    ) {
-        Spacer(modifier = Modifier.width(12.dp))
-        Text(
-            prefix,
-            fontSize = 14.sp,
-            color = ThemedColor.Editor.Selector.Normal,
-            fontFamily = JetBrainsMono
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        content(disabled)
-        Spacer(modifier = Modifier.width(12.dp))
-    }
-}
 
 @Composable
 fun BoxScope.NoFileSelected(worldName: String) {
