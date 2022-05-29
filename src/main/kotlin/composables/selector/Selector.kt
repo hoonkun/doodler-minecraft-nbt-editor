bd9c9a000feeda96cb46d26338518d38b8871f6e
hoonkun <hoonkun@unstabler.pl>
2022-05-29 10:23:55 +0900

    [feature] Tab Complete 추가 - Tab을 사용하면 후보 파일들을 순차적으로 선택할 수 있음 - 후보 파일이 선택된 상태에서 Enter 를 사용하면 해당 파일의 이름이 입력됨 - 후보 파일이 단 하나일 경우 Tab을 누르면 바로 해당 파일의 이름이 입력됨

--- src/main/kotlin/composables/selector/Selector.kt
+++ src/main/kotlin/composables/selector/Selector.kt
@@ -9,10 +9,16 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.key.*
 import androidx.compose.ui.input.pointer.isPrimaryPressed
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextOverflow
@@ -23,22 +29,74 @@ import composables.global.ThemedColor
 import java.io.File
 
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
 @Composable
 fun BoxScope.Selector(onSelect: (File) -> Unit = { }) {
 
     val basePath by remember { mutableStateOf(System.getProperty("user.home")) }
     var text by remember { mutableStateOf(TextFieldValue("/")) }
 
-    var autoCompleteFile by remember { mutableStateOf(File("$basePath${text.text}")) }
+    var ctrl by remember { mutableStateOf(false) }
+
+    var candidateParentFile by remember { mutableStateOf(File("$basePath${text.text}")) }
+    var candidateFiles by remember {
+        mutableStateOf(if (candidateParentFile.isDirectory) candidateParentFile.listFiles().toList() else listOf())
+    }
+
+    var completeTargetFile by remember { mutableStateOf<File?>(null) }
+    var completingText by remember { mutableStateOf<String?>(null) }
 
     val adjustedColumns = listOf(0, 0, 1, 0)
 
-    var selected by remember { mutableStateOf<File?>(autoCompleteFile) }
+    var selected by remember { mutableStateOf<File?>(candidateParentFile) }
+
+    val requester by remember { mutableStateOf(FocusRequester()) }
+
+
+    val updateCompletingText = {
+        val enteringFileName = text.text.substring(text.text.lastIndexOf('/') + 1, text.text.length)
+        val lastFileName = completeTargetFile!!.name
+        completingText = lastFileName.substring(enteringFileName.length until lastFileName.length)
+    }
+
+    val onTextValueUpdated: (TextFieldValue) -> Unit = {
+        val path = "$basePath${it.text}"
+        val newFile = File(path)
+        selected = newFile.let { file -> if (file.exists()) file else null }
+        if (!path.endsWith(".")) {
+            val newAutoCompleteFile = File(path.let { str -> str.substring(0, str.lastIndexOf('/')) })
+            if (newAutoCompleteFile.exists()) candidateParentFile = newAutoCompleteFile
+            candidateFiles =
+                if (candidateParentFile.isDirectory) candidateParentFile.listFiles().toList()
+                    .filter { file -> file.absolutePath.contains("$basePath${text.text}") }
+                    .sortedBy { file -> file.name }.sortedBy { file -> file.isFile }
+                else listOf()
+        }
+        completeTargetFile =
+            if (candidateFiles.size == 1) candidateFiles[0]
+            else null
+        if (completeTargetFile == null) completingText = null
+        if (candidateFiles.size == 1) {
+            updateCompletingText()
+        }
+    }
+
+    val autoComplete = autoComplete@ {
+        if (completeTargetFile == null) return@autoComplete false
+        val newValue = "${text.text}$completingText${if (completeTargetFile!!.isDirectory) "/" else ""}"
+        text = TextFieldValue(
+            newValue,
+            selection = TextRange(newValue.length)
+        )
+        onTextValueUpdated(text)
+        completeTargetFile = null
+        completingText = null
+        true
+    }
 
     Column(modifier = Modifier.fillMaxSize().padding(top = 31.dp).requiredWidthIn(min = 790.dp)) {
         Column (modifier = Modifier.padding(start = 15.dp, end = 15.dp)) {
-            Text("/** you can use '..' to go parent dir */", color = Color(0xFF629755), fontFamily = JetBrainsMono, fontSize = 18.sp)
+            Text("/** you can use '..' to go parent directory */", color = Color(0xFF629755), fontFamily = JetBrainsMono, fontSize = 18.sp)
             Spacer(modifier = Modifier.height(8.dp))
             Row(verticalAlignment = Alignment.Bottom) {
                 Text(
@@ -56,27 +114,62 @@ fun BoxScope.Selector(onSelect: (File) -> Unit = { }) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             Spacer(modifier = Modifier.width(15.dp))
-            BasicTextField(
-                text,
-                onValueChange = {
-                    text = it
-                    val path = "$basePath${it.text}"
-                    val newFile = File(path)
-                    selected = newFile.let { file -> if (file.exists()) file else null }
-                    if (!path.endsWith(".")) {
-                        val newAutoCompleteFile = File(path.let { str -> str.substring(0, str.lastIndexOf('/')) })
-                        if (newAutoCompleteFile.exists()) autoCompleteFile = newAutoCompleteFile
-                    }
-                },
-                textStyle = TextStyle(
-                    color = ThemedColor.Editor.Tag.General,
-                    fontSize = 23.sp,
-                    fontFamily = JetBrainsMono
-                ),
-                cursorBrush = SolidColor(ThemedColor.Editor.Tag.General),
-                modifier = Modifier.weight(1f),
-                singleLine = true
-            )
+            Box(modifier = Modifier.weight(1f)) {
+                BasicTextField(
+                    "${text.text}${completingText ?: ""}",
+                    onValueChange = { },
+                    readOnly = true,
+                    textStyle = TextStyle(
+                        color = ThemedColor.Editor.Tag.General,
+                        fontSize = 23.sp,
+                        fontFamily = JetBrainsMono
+                    ),
+                    cursorBrush = SolidColor(Color.Transparent),
+                    modifier = Modifier.fillMaxWidth().alpha(0.4f),
+                    singleLine = true
+                )
+                BasicTextField(
+                    text,
+                    onValueChange = {
+                        text = it
+                        onTextValueUpdated(it)
+                    },
+                    textStyle = TextStyle(
+                        color = ThemedColor.Editor.Tag.General,
+                        fontSize = 23.sp,
+                        fontFamily = JetBrainsMono
+                    ),
+                    cursorBrush = SolidColor(ThemedColor.Editor.Tag.General),
+                    modifier = Modifier.fillMaxWidth()
+                        .onKeyEvent {
+                            if (it.key == Key.Tab && it.type == KeyEventType.KeyDown) {
+                                if (completeTargetFile != null && candidateFiles.size == 1) {
+                                    autoComplete()
+                                } else {
+                                    requester.requestFocus()
+                                    if (candidateFiles.isEmpty()) return@onKeyEvent true
+                                    val noneSelected = completeTargetFile == null
+                                    val lastFile =
+                                        completeTargetFile?.absolutePath == candidateFiles.last().absolutePath
+                                    completeTargetFile =
+                                        if (noneSelected || lastFile) candidateFiles[0]
+                                        else candidateFiles[candidateFiles.indexOf(completeTargetFile) + 1]
+
+                                    val enteringFileName = text.text.substring(text.text.lastIndexOf('/') + 1, text.text.length)
+                                    val lastFileName = completeTargetFile!!.name
+                                    completingText = lastFileName.substring(enteringFileName.length until lastFileName.length)
+                                }
+                            } else if (it.key == Key.Enter && it.type == KeyEventType.KeyUp) {
+                                if (!autoComplete() && selected != null && ctrl) onSelect(selected!!)
+                            } else if (it.key == Key.CtrlLeft || it.key == Key.CtrlRight) {
+                                ctrl = it.type == KeyEventType.KeyDown
+                            }
+                            true
+                        }
+                        .focusRequester(requester),
+                    singleLine = true
+                )
+            }
             Spacer(modifier = Modifier.width(15.dp))
             Box (
                 modifier = Modifier.width(80.dp).fillMaxHeight().padding(5.dp)
@@ -107,15 +200,14 @@ fun BoxScope.Selector(onSelect: (File) -> Unit = { }) {
                     entire.indexOf(chunked) == 3
                 }
 
-            if (autoCompleteFile.isDirectory) {
-                val children = autoCompleteFile.listFiles()
+            if (candidateParentFile.isDirectory) {
                 val columns = 4
-                val childDirectories = children
-                    .filter { it.isDirectory && it.absolutePath.contains("$basePath${text.text}") }
+                val childDirectories = candidateFiles
+                    .filter { it.isDirectory }
                     .sortedBy { it.name }
                     .chunked(columns)
-                val childFiles = children
-                    .filter { it.isFile && it.absolutePath.contains("$basePath${text.text}") }
+                val childFiles = candidateFiles
+                    .filter { it.isFile }
                     .sortedBy { it.name }
                     .chunked(columns)
 
