9d9a440cf708b2e2c6442e140e12f4842bf09757
hoonkun <hoonkun@unstabler.pl>
2022-06-02 16:46:07 +0900

    [debugging](DoodlerLogger) 메시지 필터 기능을 추가함

--- src/main/kotlin/doodler/logger/DoodlerLogger.kt
+++ src/main/kotlin/doodler/logger/DoodlerLogger.kt
@@ -9,9 +9,10 @@ class DoodlerLogger {
         private const val tagLength = 12
 
         private const val TEST_TAG = "TEST"
-        const val RECOMPOSITION_TAG = "RECOMPOSITION"
+        private const val RECOMPOSITION_TAG = "RECOMPOSITION"
 
-        val tagFilters = mutableSetOf(TEST_TAG, RECOMPOSITION_TAG)
+        private val tagFilters = mutableSetOf<String>(TEST_TAG, RECOMPOSITION_TAG)
+        private val messageFilters = mutableSetOf<String>()
 
         private fun now(): String {
             val h = Calendar.getInstance()[Calendar.HOUR].toString().padStart(2, '0')
@@ -26,13 +27,13 @@ class DoodlerLogger {
         }
 
         fun test(message: String) {
-            if (!tagFilters.contains(TEST_TAG)) return
+            if (!tagFilters.contains(TEST_TAG) || messageFilters.find { message.contains(it) } != null) return
 
             log(TEST_TAG, message)
         }
 
         fun recomposition(from: String) {
-            if (!tagFilters.contains(RECOMPOSITION_TAG)) return
+            if (!tagFilters.contains(RECOMPOSITION_TAG) || messageFilters.find { from.contains(it) } != null) return
 
             log(RECOMPOSITION_TAG, from)
         }
