d34dc449426d203935a1f6a7398aa32632362082
hoonkun <hoonkun@unstabler.pl>
2022-06-06 23:24:35 +0900

    [checkpoint] 컴포저블에서 사용할 상태 제공 람다를 typealias로 재정의함

---
+++ src/main/kotlin/doodler/types/Provider.kt
@@ -0,0 +1,8 @@
+package doodler.types
+
+typealias Provider<T> = () -> T
+typealias Mapper<T, R> = (T) -> R
+typealias BooleanProvider = () -> Boolean
+
+val TrueProvider: BooleanProvider = { true }
+val FalseProvider: BooleanProvider = { false }
