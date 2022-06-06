29ca29126d0d56710771a2eeaabc8e134111b833
hoonkun <hoonkun@unstabler.pl>
2022-06-06 20:00:51 +0900

    [checkpoint] 기존의 WorldEditorState 를 재사용

---
+++ src/main/kotlin/doodler/editor/WorldEditorState.kt
@@ -0,0 +1,19 @@
+package doodler.editor
+
+import activator.doodler.minecraft.structures.WorldSpecification
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
+
+class WorldEditorState (
+    val manager: EditorManager,
+    val worldSpec: WorldSpecification
+)
+
+@Composable
+fun rememberWorldEditorState (
+    manager: EditorManager = EditorManager(),
+    worldSpec: WorldSpecification = WorldSpecification()
+) = remember (manager, worldSpec) {
+    WorldEditorState(manager, worldSpec)
+}
