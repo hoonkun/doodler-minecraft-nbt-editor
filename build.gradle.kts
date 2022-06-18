ca2ed4f4dbe0fc64d46ff9dca6c436baf01e28ff
hoonkun <hoonkun@unstabler.pl>
2022-06-18 16:43:15 +0900

    [refactor] Kotlin의 버전에 맞게 Compose 버전도 변경, 다만 그렇게 해도 Compose 는 지원하는 Kotlin의 버전이 낮아 일단은 지원하는 버전으로 내림

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.21"
-    id("org.jetbrains.compose") version "1.0.0"
+    id("org.jetbrains.compose") version "1.1.0"
 }
 
 group = "hoonkun.kiwi"
