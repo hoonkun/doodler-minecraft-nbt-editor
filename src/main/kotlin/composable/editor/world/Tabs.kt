ffcea1c1e28139e1f3b7292f17782552c21e635c
hoonkun <hoonkun@unstabler.pl>
2022-06-22 14:11:16 +0900

    [feature] 드래그/드롭으로 열린 탭의 순서를 변경할 수 있도록 함

--- src/main/kotlin/composable/editor/world/Tabs.kt
+++ src/main/kotlin/composable/editor/world/Tabs.kt
@@ -1,22 +1,25 @@
 package composable.editor.world
 
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.zIndex
 import doodler.editor.AnvilNbtEditor
 import doodler.editor.Editor
@@ -26,6 +29,7 @@ import doodler.theme.DoodlerTheme
 import doodler.types.BooleanProvider
 import doodler.unit.ddp
 import doodler.unit.dsp
+import kotlinx.coroutines.CoroutineScope
 
 
 private val TextStyle.fsp get() = this.fontSize * 0.9f
@@ -37,6 +41,30 @@ fun EditorTabGroup(
     onCloseTab: (Editor) -> Unit
 ) {
     val scrollState = rememberScrollState()
+
+    val tabWidths = remember { mutableStateListOf<Int>() }
+
+    val onCalculateTabWidth: (Editor, Int) -> Unit = { editor, width ->
+        while (tabWidths.size < manager.editors.size) { tabWidths.add(0) }
+        tabWidths[manager.editors.indexOf(editor)] = width
+    }
+
+    val onTabDrag: (Editor, Float) -> Float = lambda@ { editor, offset ->
+        val index = manager.editors.indexOf(editor)
+        if (index > 0 && tabWidths[index - 1] / 2f < -offset) {
+            val offsetDiff = tabWidths[index - 1] / 2f
+            manager.editors.add(index - 1, manager.editors.removeAt(index))
+            tabWidths.add(index - 1, tabWidths.removeAt(index))
+            return@lambda offsetDiff
+        }
+        if (index < manager.editors.lastIndex && tabWidths[index + 1] / 2f < offset) {
+            val offsetDiff = tabWidths[index + 1] / 2f
+            manager.editors.add(index + 1, manager.editors.removeAt(index))
+            tabWidths.add(index + 1, tabWidths.removeAt(index))
+            return@lambda offsetDiff - tabWidths[index]
+        }
+        offset
+    }
 
     Box (
         modifier = Modifier
@@ -50,12 +78,16 @@ fun EditorTabGroup(
                 .horizontalScroll(scrollState)
         ) {
             for (editor in manager.editors) {
-                EditorTab(
-                    editor = editor,
-                    selectedProvider = { manager.selected == editor },
-                    onSelectTab = onSelectTab,
-                    onCloseTab = onCloseTab
-                )
+                key(editor.ident) {
+                    EditorTab(
+                        editor = editor,
+                        selectedProvider = { manager.selected == editor },
+                        onSelectTab = onSelectTab,
+                        onCloseTab = onCloseTab,
+                        onTabDrag = onTabDrag,
+                        onCalculateTabWidth = onCalculateTabWidth
+                    )
+                }
             }
         }
     }
@@ -66,7 +98,9 @@ fun EditorTab(
     editor: Editor,
     selectedProvider: BooleanProvider,
     onSelectTab: (Editor) -> Unit,
-    onCloseTab: (Editor) -> Unit
+    onCloseTab: (Editor) -> Unit,
+    onTabDrag: (Editor, Float) -> Float,
+    onCalculateTabWidth: (Editor, Int) -> Unit
 ) {
     val hoverInteractionSource = remember { MutableInteractionSource() }
     val pressInteractionSource = remember { MutableInteractionSource() }
@@ -74,12 +108,31 @@ fun EditorTab(
     val hovered by hoverInteractionSource.collectIsHoveredAsState()
     val pressed by pressInteractionSource.collectIsPressedAsState()
 
+    val offset = remember { mutableStateOf(0f) }
+    val draggableState = rememberDraggableState {
+        offset.value = onTabDrag(editor, offset.value + it)
+    }
+
+    val onStopDrag: suspend CoroutineScope.(Float) -> Unit = {
+        offset.value = 0f
+    }
+
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .hoverable(hoverInteractionSource)
             .clickable(pressInteractionSource, null) { onSelectTab(editor) }
+            .offset { IntOffset(x = offset.value.toInt(), y = 0) }
+            .zIndex(if (offset.value != 0f) 10f else 0f)
+            .onGloballyPositioned { onCalculateTabWidth(editor, it.size.width) }
+            .draggable(
+                state = draggableState,
+                orientation = Orientation.Horizontal,
+                enabled = true,
+                onDragStopped = onStopDrag
+            )
             .drawBehind {
+                drawRect(DoodlerTheme.Colors.SecondaryBackground)
                 drawRect(DoodlerTheme.Colors.Editor.Tab(selectedProvider(), pressed, hovered))
 
                 if (!selectedProvider()) return@drawBehind
