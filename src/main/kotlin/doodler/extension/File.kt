cd7362cbe5da278e8c0e46f8e4c7fe7ebbe5e8f9
hoonkun <hoonkun@unstabler.pl>
2022-07-03 19:48:19 +0900

    [fix] 서버 월드의 아이콘이 Intro 윈도우의 최근 월드에 표시되지 않던 문제 수정

--- src/main/kotlin/doodler/extension/File.kt
+++ src/main/kotlin/doodler/extension/File.kt
@@ -5,3 +5,5 @@ import java.io.File
 fun File.safeListFiles(): List<File> {
     return this.let { if (!it.exists()) null else it.listFiles()?.toList() } ?: listOf()
 }
+
+fun fileOrNull(path: String) = File(path).let { if (it.exists()) it else null }
